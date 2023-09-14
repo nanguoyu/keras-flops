3e4aaa85d3fac926de22a5295aab74bd8bf219de
Dong Wang <dongwang@wangdongdong.wang>
2023-09-14 23:02:03 +0200

    fix double registration in tf 2.13

--- keras_flops/flops_registory.py
+++ keras_flops/flops_registory.py
@@ -6,37 +6,42 @@ from tensorflow.python.profiler.internal.flops_registry import (
     _binary_per_element_op_flops,
 )
 
+try:
+    @ops.RegisterStatistics("FusedBatchNormV3", "flops")
+    def _flops_fused_batch_norm_v3(graph, node):
+        """inference is only supportted"""
+        in_shape = graph_util.tensor_shape_from_node_def_name(graph, node.input[0])
+        in_shape.assert_is_fully_defined()
+        mean_shape = graph_util.tensor_shape_from_node_def_name(graph, node.input[3])
+        mean_shape.assert_is_fully_defined()
+        variance_shape = graph_util.tensor_shape_from_node_def_name(graph, node.input[4])
+        variance_shape.assert_is_fully_defined()
 
-@ops.RegisterStatistics("FusedBatchNormV3", "flops")
-def _flops_fused_batch_norm_v3(graph, node):
-    """inference is only supportted"""
-    in_shape = graph_util.tensor_shape_from_node_def_name(graph, node.input[0])
-    in_shape.assert_is_fully_defined()
-    mean_shape = graph_util.tensor_shape_from_node_def_name(graph, node.input[3])
-    mean_shape.assert_is_fully_defined()
-    variance_shape = graph_util.tensor_shape_from_node_def_name(graph, node.input[4])
-    variance_shape.assert_is_fully_defined()
+        if node.attr["is_training"].b is True:
+            raise ValueError("Only supports inference mode")
 
-    if node.attr["is_training"].b is True:
-        raise ValueError("Only supports inference mode")
+        num_flops = (
+            2 * in_shape.num_elements()
+            + 5 * variance_shape.num_elements()
+            + mean_shape.num_elements()
+        )
+        return ops.OpStats("flops", num_flops)
+except KeyError:
+    pass
 
-    num_flops = (
-        2 * in_shape.num_elements()
-        + 5 * variance_shape.num_elements()
-        + mean_shape.num_elements()
-    )
-    return ops.OpStats("flops", num_flops)
+try:
+    @ops.RegisterStatistics("Max", "flops")
+    def _flops_max(graph, node):
+        """inference is supportted"""
+        # reduction - comparison, no finalization
+        return _reduction_op_flops(graph, node, reduce_flops=1, finalize_flops=0)
+except KeyError:
+    pass
 
-
-@ops.RegisterStatistics("Max", "flops")
-def _flops_max(graph, node):
-    """inference is supportted"""
-    # reduction - comparison, no finalization
-    return _reduction_op_flops(graph, node, reduce_flops=1, finalize_flops=0)
-
-
-@ops.RegisterStatistics("AddV2", "flops")
-def _flops_add(graph, node):
-    """inference is supportted"""
-    return _binary_per_element_op_flops(graph, node)
-
+try:
+    @ops.RegisterStatistics("AddV2", "flops")
+    def _flops_add(graph, node):
+        """inference is supportted"""
+        return _binary_per_element_op_flops(graph, node)
+except KeyError:
+    pass
